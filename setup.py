d636f9ce38787be0e6f66799c848ff4b045a4468
Erik Blaufuss <blaufuss@morbo.hsd1.md.comcast.net>
2020-04-22 11:21:16 -0400

    Make setup.py simpler to ensure things work in prod

--- setup.py
+++ setup.py
@@ -2,19 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VOEventLib',
-    version='1.3_dev',
-    url='https://git.ligo.org/emfollow/VOEventLib',
-    description='Python library to read, modify, and create VOEvents',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    classifiers=(
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Topic :: Internet",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Topic :: Scientific/Engineering :: Physics"
-    ),
+    version='1.3',
     packages=find_packages(),
     python_requires='>=3.5',
 )
